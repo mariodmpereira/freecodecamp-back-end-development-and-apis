c9fce9f939eba689efb0a3ab49e8b60bb65507aa
mariodmpereira <mariodmpereira@gmail.com>
2021-11-13 20:01:47 +0000

    Clone 'MongoDB and Mongoose'

---
+++ 03-mongodb-and-mongoose/myApp.js
@@ -0,0 +1,70 @@
+require('dotenv').config();
+
+
+let Person;
+
+const createAndSavePerson = (done) => {
+  done(null /*, data*/);
+};
+
+const createManyPeople = (arrayOfPeople, done) => {
+  done(null /*, data*/);
+};
+
+const findPeopleByName = (personName, done) => {
+  done(null /*, data*/);
+};
+
+const findOneByFood = (food, done) => {
+  done(null /*, data*/);
+};
+
+const findPersonById = (personId, done) => {
+  done(null /*, data*/);
+};
+
+const findEditThenSave = (personId, done) => {
+  const foodToAdd = "hamburger";
+
+  done(null /*, data*/);
+};
+
+const findAndUpdate = (personName, done) => {
+  const ageToSet = 20;
+
+  done(null /*, data*/);
+};
+
+const removeById = (personId, done) => {
+  done(null /*, data*/);
+};
+
+const removeManyPeople = (done) => {
+  const nameToRemove = "Mary";
+
+  done(null /*, data*/);
+};
+
+const queryChain = (done) => {
+  const foodToSearch = "burrito";
+
+  done(null /*, data*/);
+};
+
+/** **Well Done !!**
+/* You completed these challenges, let's go celebrate !
+ */
+
+//----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
+
+exports.PersonModel = Person;
+exports.createAndSavePerson = createAndSavePerson;
+exports.findPeopleByName = findPeopleByName;
+exports.findOneByFood = findOneByFood;
+exports.findPersonById = findPersonById;
+exports.findEditThenSave = findEditThenSave;
+exports.findAndUpdate = findAndUpdate;
+exports.createManyPeople = createManyPeople;
+exports.removeById = removeById;
+exports.removeManyPeople = removeManyPeople;
+exports.queryChain = queryChain;
