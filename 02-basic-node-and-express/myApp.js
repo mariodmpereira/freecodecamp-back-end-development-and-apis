90217bd527d05117d1bdc843c0d795564e0c7242
mariodmpereira <mariodmpereira@gmail.com>
2021-11-13 12:06:58 +0000

    Use the .env File

--- 02-basic-node-and-express/myApp.js
+++ 02-basic-node-and-express/myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 var express = require('express');
 var app = express();
 
@@ -6,9 +7,9 @@ app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
-    res.json({
-        message: "Hello json"
-    });
+    const json = {message: "Hello json"};
+    json.message = process.env.MESSAGE_STYLE === "uppercase" ? json.message.toUpperCase() : json.message;
+    res.json(json);
 });
 
 module.exports = app;
