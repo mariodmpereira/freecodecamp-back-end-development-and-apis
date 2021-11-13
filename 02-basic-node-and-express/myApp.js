779265fc9beaaf15b215ca484ee4c61cb28ec164
mariodmpereira <mariodmpereira@gmail.com>
2021-11-13 11:50:21 +0000

    Serve JSON on a Specific Route

--- 02-basic-node-and-express/myApp.js
+++ 02-basic-node-and-express/myApp.js
@@ -5,4 +5,10 @@ app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
 app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/json", (req, res) => {
+    res.json({
+        message: "Hello json"
+    });
+});
+
 module.exports = app;
