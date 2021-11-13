3292250c39fad221ae5f66c2fb54bdf72249eed4
mariodmpereira <mariodmpereira@gmail.com>
2021-11-13 14:26:19 +0000

    Complete 'Chain Middleware to Create a Time Server'

--- 02-basic-node-and-express/myApp.js
+++ 02-basic-node-and-express/myApp.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 var express = require('express');
 var app = express();
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     console.log(`${req.method} ${req.path} - ${req.ip}`);
     next();
 })
@@ -12,11 +12,16 @@ app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
-    const json = {message: "Hello json"};
+    const json = { message: "Hello json" };
     json.message = process.env.MESSAGE_STYLE === "uppercase" ? json.message.toUpperCase() : json.message;
     res.json(json);
 });
 
-
+app.get("/now", (req, res, next) => {
+        req.time = new Date().toString();
+        next();
+    },
+    (req, res) => res.json({ time: req.time })
+);
 
 module.exports = app;
