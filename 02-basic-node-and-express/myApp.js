29ea7fabecabed213b1cc5363efe8a2c5da696bb
mariodmpereira <mariodmpereira@gmail.com>
2021-11-13 11:25:22 +0000

    Server an HTML File

--- 02-basic-node-and-express/myApp.js
+++ 02-basic-node-and-express/myApp.js
@@ -1,39 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.get("/", (req, res) => res.send("Hello Express"));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
  module.exports = app;
