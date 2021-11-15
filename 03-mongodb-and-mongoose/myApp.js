08a644f2bf848f0976e5212964183deedce72754
mariodmpereira <mariodmpereira@gmail.com>
2021-11-15 17:42:13 +0000

    Complete 'Use model.find() to Search Your Database'

--- 03-mongodb-and-mongoose/myApp.js
+++ 03-mongodb-and-mongoose/myApp.js
@@ -26,7 +26,7 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-    done(null /*, data*/);
+    Person.find({name: personName}, (err, data) => err ? done(err) : done(null, data));
 };
 
 const findOneByFood = (food, done) => {
