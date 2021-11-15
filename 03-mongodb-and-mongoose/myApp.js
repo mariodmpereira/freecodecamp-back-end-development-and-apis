0185f6c47a38a6a37ab907d57d10f582965fa04e
mariodmpereira <mariodmpereira@gmail.com>
2021-11-15 17:17:52 +0000

    Complete 'Create and Save a Record of a Model'

--- 03-mongodb-and-mongoose/myApp.js
+++ 03-mongodb-and-mongoose/myApp.js
@@ -4,66 +4,67 @@ const mongoose = require('mongoose');
 mongoose.connect(envs.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const personSchema = {
-  name: {
-    type: String,
-    required: true
-  },
-  age: Number,
-  favoriteFoods: [String]
+    name: {
+        type: String,
+        required: true
+    },
+    age: Number,
+    favoriteFoods: [String]
 }
 
 let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+    const person = new Person({ name: 'Super Mario', age: 40, favoriteFoods: ['Spaghetti'] });
+    person.save((err, data) => err ? done(err) : done(null, data));
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const findEditThenSave = (personId, done) => {
-  const foodToAdd = "hamburger";
+    const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const findAndUpdate = (personName, done) => {
-  const ageToSet = 20;
+    const ageToSet = 20;
 
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const removeManyPeople = (done) => {
-  const nameToRemove = "Mary";
+    const nameToRemove = "Mary";
 
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const queryChain = (done) => {
-  const foodToSearch = "burrito";
+    const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 /** **Well Done !!**
-/* You completed these challenges, let's go celebrate !
+ /* You completed these challenges, let's go celebrate !
  */
 
 //----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is my path on this Javascript certification.
 
 Please be aware that there is nothing here other than spoilers. After all, this is my solution to all the exercises on this certification.
 
-Completed 0/4.
+Completed 2/4.
 
 Cheers.
 
