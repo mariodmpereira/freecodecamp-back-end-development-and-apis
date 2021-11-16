a3253162e9f0436a9f120b2bc302c476575b1ed9
mariodmpereira <mariodmpereira@gmail.com>
2021-11-16 20:40:54 +0000

    Complete 'Chain Search Query Helpers to Narrow Search Results'

--- 03-mongodb-and-mongoose/myApp.js
+++ 03-mongodb-and-mongoose/myApp.js
@@ -66,8 +66,11 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
     const foodToSearch = "burrito";
-
-    done(null /*, data*/);
+    Person.find({ favoriteFoods: foodToSearch })
+        .sort({ name: 1 })
+        .limit(2)
+        .select({ age: 0 })
+        .exec((err, deletedPersons) => (err) ? done(err) : done(null, deletedPersons));
 };
 
 /** **Well Done !!**
