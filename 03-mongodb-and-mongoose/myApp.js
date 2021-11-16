8aebdae483a4bfd6a3c08656e0ee67b13b4f8561
mariodmpereira <mariodmpereira@gmail.com>
2021-11-16 20:17:13 +0000

    Complete 'Perform New Updates on a Document Using model.findOneAndUpdate()'

--- 03-mongodb-and-mongoose/myApp.js
+++ 03-mongodb-and-mongoose/myApp.js
@@ -50,7 +50,9 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
     const ageToSet = 20;
 
-    done(null /*, data*/);
+    Person.findOneAndUpdate({ name: personName }, { age: ageToSet }, { new: true }, (err, updatedPerson) =>
+        (err) ? done(err) : done(null, updatedPerson)
+    )
 };
 
 const removeById = (personId, done) => {
