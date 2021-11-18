e23ba1e5dff672ef189124d53e5c0523300de516
mariodmpereira <mariodmpereira@gmail.com>
2021-11-18 23:39:29 +0000

    Complete 'Timestamp Microservice'

--- 04-back-end-development-and-apis-projects/01-timestamp-microservice/server.js
+++ 04-back-end-development-and-apis-projects/01-timestamp-microservice/server.js
@@ -8,25 +8,41 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const url = require("url");
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+    res.json({ greeting: 'hello API' });
 });
 
+app.get("/api", (req, res) => {
+    const now = new Date();
+    res.json({ unix: now.getTime(), utc: now.toUTCString() })
+});
 
+app.get("/api/:date", (req, res) => {
+    const paramsDate = req.params.date;
+    const invalidDate = "Invalid Date";
+    const date = parseInt(paramsDate) < 10000
+        ? new Date(paramsDate)
+        : new Date(parseInt(paramsDate))
+
+    date.toString() === invalidDate
+        ? res.json({ error: invalidDate })
+        : res.json({ unix: date.valueOf(), utc: date.toUTCString() });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+    console.log('Your app is listening on port ' + listener.address().port);
 });
