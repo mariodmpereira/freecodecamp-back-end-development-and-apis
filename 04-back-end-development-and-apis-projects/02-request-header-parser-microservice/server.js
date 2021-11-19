2b7449c5bcfe126d5782e9ad18117997c8595a1f
mariodmpereira <mariodmpereira@gmail.com>
2021-11-19 01:42:56 +0000

    Complete 'Request Header Parser Microservice'

--- 04-back-end-development-and-apis-projects/02-request-header-parser-microservice/server.js
+++ 04-back-end-development-and-apis-projects/02-request-header-parser-microservice/server.js
@@ -9,25 +9,31 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+    res.json({ greeting: 'hello API' });
 });
 
-
+app.get("/api/whoami", (req, res) => {
+    res.json({
+        "ipaddress": req.socket.remoteAddress,
+        "language": req.get('accept-language'),
+        "software": req.get('user-agent')
+    });
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+    console.log('Your app is listening on port ' + listener.address().port);
 });
