720b2572b08c1a509fcea92b7e31c0504edf085d
mariodmpereira <mariodmpereira@gmail.com>
2021-11-27 20:13:26 +0000

    Complete the last exercise of this certification, 'File Metadata MicroservicePassed'

--- 04-back-end-development-and-apis-projects/05-file-metadata-microservice/server.js
+++ 04-back-end-development-and-apis-projects/05-file-metadata-microservice/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer')
 require('dotenv').config()
 
 var app = express();
@@ -11,10 +12,15 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
+    res.json({
+        name: req.file.originalname,
+        type: req.file.mimetype,
+        size: req.file.size
+    });
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+    console.log('Your app is listening on port ' + port)
 });
